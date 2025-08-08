c6dba5d7e207754aea9e34a8b42e2530b50fe0d3
eswardattasai <dattamessi10@gmail.com>
2025-08-08 16:56:06 +0530

    added backend fetch

--- audiovoice-chat/src/components/ChatInterface.tsx
+++ audiovoice-chat/src/components/ChatInterface.tsx
@@ -66,7 +66,7 @@ export const ChatInterface = () => {
     setMessages(prev => [...prev, newMessage]);
 
     try {
-      const res = await fetch('http://127.0.0.1:8000/ask', {
+      const res = await fetch(' https://plivo-2.onrender.com/ask', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
